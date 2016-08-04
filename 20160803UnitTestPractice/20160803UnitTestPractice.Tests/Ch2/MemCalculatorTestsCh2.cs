c814c60a7eea89abfe4290b92d491c37eb009827
sc9j <Stella.W.Chen@newegg.com>
2016-08-04 16:42:19 +0800

    2.7 add MemCalculatorTest demo

---
+++ 20160803UnitTestPractice/20160803UnitTestPractice.Tests/Ch2/MemCalculatorTestsCh2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace _20160803UnitTestPractice.Tests
+{
+    [TestFixture]
+    public class MemCalculatorTestsCh2
+    {
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculatorCh2 calc = MackeCalc();
+            int lastSum = calc.Sum();
+            Assert.AreEqual(0, lastSum);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangeSum()
+        {
+            MemCalculatorCh2 calc = MackeCalc();
+            calc.Add(1);
+            int lastSum = calc.Sum();
+            Assert.AreEqual(1, lastSum);
+        }
+
+        private static MemCalculatorCh2 MackeCalc()
+        {
+            return new MemCalculatorCh2();
+        }
+    }
+}
---
+++ 20160803UnitTestPractice/20160803UnitTestPractice/Ch2/MemCalculatorCh2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _20160803UnitTestPractice
+{
+    public class MemCalculatorCh2
+    {
+        private int sum = 0;
+
+        public void Add(int num)
+        {
+            sum += num;
+        }
+
+        public int Sum()
+        {
+            int temp = sum;
+            sum = 0;
+            return temp;
+        }
+    }
+}
