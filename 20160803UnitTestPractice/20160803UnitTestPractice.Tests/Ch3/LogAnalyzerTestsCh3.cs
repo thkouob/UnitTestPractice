cbed678994400903ec4e30fb6e26e9769429d5e1
sc9j <Stella.W.Chen@newegg.com>
2016-08-05 15:57:46 +0800

    3.4.4 using fake object simulate exception

--- 20160803UnitTestPractice/20160803UnitTestPractice.Tests/Ch3/LogAnalyzerTestsCh3.cs
+++ 20160803UnitTestPractice/20160803UnitTestPractice.Tests/Ch3/LogAnalyzerTestsCh3.cs
@@ -18,5 +18,16 @@ namespace _20160803UnitTestPractice.Tests
             bool result = log.IsValidLogFileName("short.ext");
             Assert.IsTrue(result);
         }
+
+        [Test]
+        public void IsValidFileName_ExtManagerThrowsException_ReturnsFalse()
+        {
+            FakeExtensionManager myFakeManager = new FakeExtensionManager();
+            myFakeManager.WillThrow = new Exception("this is fake");
+
+            LogAnalyzerCh3 log = new LogAnalyzerCh3(myFakeManager);
+            bool result = log.IsValidLogFileName("anything.anyextension");
+            Assert.IsFalse(result);
+        }
     }
 }
--- 20160803UnitTestPractice/20160803UnitTestPractice/Ch3/FakeExtensionManager.cs
+++ 20160803UnitTestPractice/20160803UnitTestPractice/Ch3/FakeExtensionManager.cs
@@ -4,9 +4,15 @@ namespace _20160803UnitTestPractice
     public class FakeExtensionManager : IExtensionManager
     {
         public bool WillBeValid = false;
+        public Exception WillThrow = null;
 
         public bool IsValid(string fileName)
         {
+            if (WillThrow != null) 
+            {
+                throw WillThrow;
+            }
+
             return WillBeValid;
         }
     }
